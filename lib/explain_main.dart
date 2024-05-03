d78e08cf76d08cc4de7125423baa9db825db71ec
lyuhyewon <lyuhw11023@gmail.com>
2024-05-03 21:26:13 +0900

    explain_main 페이지 디자인 수정

--- lib/explain_main.dart
+++ lib/explain_main.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 
 class ExplainMain extends StatelessWidget{
@@ -18,35 +16,35 @@ class ExplainMain extends StatelessWidget{
             children: <Widget>[
               Text(
                 '카메라를 편의점 물건들에 비추면,',
-                style: TextStyle(fontSize: 23),
+                style: TextStyle(fontSize: 18),
               ),
               Text(
                 '그 물건이 무엇인지',
-                style: TextStyle(fontSize: 23),
+                style: TextStyle(fontSize: 18),
               ),
               Text(
                 '자동으로 읽을 수 있습니다.\n',
-                style: TextStyle(fontSize: 23),
+                style: TextStyle(fontSize: 18),
               ),
               Text(
                 '읽고자 하는 물건이',
-                style: TextStyle(fontSize: 23),
+                style: TextStyle(fontSize: 18),
               ),
               Text(
                 '카메라 프레임에 걸치거나,',
-                style: TextStyle(fontSize: 23),
+                style: TextStyle(fontSize: 18),
               ),
               Text(
                 '잘 안보이는 경우,',
-                style: TextStyle(fontSize: 23),
+                style: TextStyle(fontSize: 18),
               ),
               Text(
                 '진동을 주어 프레임 안쪽으로',
-                style: TextStyle(fontSize: 23),
+                style: TextStyle(fontSize: 18),
               ),
               Text(
                 '들어오도록 할 수 있습니다.',
-                style: TextStyle(fontSize: 23),
+                style: TextStyle(fontSize: 18),
               ),
             ],
           )
